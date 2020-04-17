148ac703e9fa8375a734464eb6cfef0595302c32
Dhruv-RawPower <dhruv10.shah@gmail.com>
2020-04-17 17:40:23 +0530

    save changes

---
+++ Source2.c
@@ -0,0 +1,9 @@
+//#include<stdio.h>
+//void main()
+//{
+//	int a, b;
+//	a = 5;
+//	b = 10;
+//	printf("%d",a+b);
+//	getch();
+//}
---
+++ Source7.c
@@ -0,0 +1,10 @@
+//#include<stdio.h>
+//#include<conio.h>
+//void main()
+//{
+//	int a, b;
+//	printf("Enter 2 numbers");
+//	scanf_s("%d %d",&a,&b);
+//	printf("%d", a+b);
+//	_getch();
+//}
